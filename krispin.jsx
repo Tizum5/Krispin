0d2b41a44f89f85e3e842a15d3d23a347b769b0a
Tizum5 <88816813+Tizum5@users.noreply.github.com>
2021-12-05 03:30:55 -0500

    Design, UI possitions, base for React APP

---
+++ krispin.jsx
@@ -0,0 +1,257 @@
+const background = {
+  width: 1500,
+  height: 75,
+  backgroundColor: "rgba(39, 0, 18, 0.01)",
+  overflow: "hidden",
+}
+
+const backgroundImg = {
+  width: 1500,
+  height: 1000,
+  overflow: "hidden",
+  backgroundImage: "url(/* background-image.jpg */)",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+}
+
+const navbar = {
+  width: 1500,
+  height: 75,
+  backgroundColor: "#330033",
+  overflow: "hidden",
+}
+
+const lowerNav = {
+  width: 1500,
+  height: 71,
+  backgroundColor: "#330133",
+  overflow: "hidden",
+}
+
+const connectWalBtn = {
+  width: 148,
+  height: 43,
+  backgroundColor: "#800080",
+  overflow: "hidden",
+}
+
+const connectWalletText = {
+  width: 142,
+  height: 24,
+  overflow: "visible",
+  whiteSpace: "pre-wrap",
+  wordWrap: "break-word",
+  wordBreak: "break-word",
+  fontWeight: 400,
+  fontStyle: "normal",
+  fontFamily: `"ZCOOL QingKe HuangYou", serif`,
+  color: "#ffd700",
+  fontSize: 24,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+  textAlign: "center",
+}
+
+const coinsBtn = {
+  width: 148,
+  height: 43,
+  backgroundColor: "#800080",
+  overflow: "hidden",
+}
+
+const coinsText = {
+  width: 160,
+  height: 43,
+  overflow: "visible",
+  whiteSpace: "pre-wrap",
+  wordWrap: "break-word",
+  wordBreak: "break-word",
+  textShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+  fontWeight: 400,
+  fontStyle: "normal",
+  fontFamily: `"ZCOOL QingKe HuangYou", serif`,
+  color: "#ffd700",
+  fontSize: 24,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+  textAlign: "center",
+}
+
+const supportText = {
+  width: 125,
+  height: 43,
+  overflow: "visible",
+  whiteSpace: "pre-wrap",
+  wordWrap: "break-word",
+  wordBreak: "break-word",
+  fontWeight: 400,
+  fontStyle: "normal",
+  fontFamily: `"ZCOOL QingKe HuangYou", serif`,
+  color: "#bbbbbb",
+  fontSize: 24,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+  textAlign: "center",
+}
+
+const logoBackground = {
+  boxSizing: "border-box",
+  width: 302,
+  height: 59,
+  boxShadow: "0px 4px 4px 0px rgba(0, 0, 0, 0.25)",
+  backgroundColor: "#530f53",
+  overflow: "hidden",
+  border: "1px solid #000000",
+}
+
+const logoText = {
+  width: 355,
+  height: 59,
+  overflow: "visible",
+  whiteSpace: "pre-wrap",
+  wordWrap: "break-word",
+  wordBreak: "break-word",
+  fontWeight: 400,
+  fontStyle: "normal",
+  fontFamily: `"ZCOOL QingKe HuangYou", serif`,
+  color: "#ffd700",
+  fontSize: 62,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+  textAlign: "center",
+}
+
+const banner = {
+  width: 1500,
+  height: 163,
+  overflow: "hidden",
+  backgroundImage: "url(/* background-image.jpg */)",
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+}
+
+const lowerLogoText = {
+  width: 209,
+  height: 46,
+  overflow: "visible",
+  whiteSpace: "pre-wrap",
+  wordWrap: "break-word",
+  wordBreak: "break-word",
+  fontWeight: 400,
+  fontStyle: "normal",
+  fontFamily: `"ZCOOL QingKe HuangYou", serif`,
+  color: "#ffd700",
+  fontSize: 62,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+  textAlign: "center",
+}
+
+const line = {
+  width: 67,
+  height: 8,
+  transform: "rotate(-90deg)",
+}
+
+const textSupportBtnBtm = {
+  width: 226,
+  height: 53,
+  overflow: "visible",
+  whiteSpace: "pre-wrap",
+  wordWrap: "break-word",
+  wordBreak: "break-word",
+  fontWeight: 400,
+  fontStyle: "normal",
+  fontFamily: `"ZCOOL QingKe HuangYou", serif`,
+  color: "#9a861a",
+  fontSize: 62,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+  textAlign: "center",
+}
+
+const privacyBtnTxt = {
+  width: 244,
+  height: 58,
+  overflow: "visible",
+  whiteSpace: "pre-wrap",
+  wordWrap: "break-word",
+  wordBreak: "break-word",
+  fontWeight: 400,
+  fontStyle: "normal",
+  fontFamily: `"ZCOOL QingKe HuangYou", serif`,
+  color: "#9a861a",
+  fontSize: 62,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+  textAlign: "center",
+}
+
+const TermsOfServiceBtnText = {
+  width: 382,
+  height: 66,
+  overflow: "visible",
+  whiteSpace: "pre-wrap",
+  wordWrap: "break-word",
+  wordBreak: "break-word",
+  fontWeight: 400,
+  fontStyle: "normal",
+  fontFamily: `"ZCOOL QingKe HuangYou", serif`,
+  color: "#9a861a",
+  fontSize: 62,
+  letterSpacing: 0,
+  lineHeight: 1.2,
+  textAlign: "center",
+}
+
+const discordIcon = {
+  width: 50,
+  height: 50,
+  overflow: "hidden",
+  backgroundImage: "url(/* background-image.jpg */)",
+  backgroundSize: "contain",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+}
+
+const gitHubIcon = {
+  width: 50,
+  height: 50,
+  overflow: "hidden",
+  backgroundImage: "url(/* background-image.jpg */)",
+  backgroundSize: "contain",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+}
+
+const telegramAppIcon = {
+  width: 50,
+  height: 50,
+  overflow: "hidden",
+  backgroundImage: "url(/* background-image.jpg */)",
+  backgroundSize: "contain",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+}
+
+const mediumIcon = {
+  width: 50,
+  height: 50,
+  overflow: "hidden",
+  backgroundImage: "url(/* background-image.jpg */)",
+  backgroundSize: "contain",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+}
+
+const twitterIcon = {
+  width: 50,
+  height: 50,
+  overflow: "hidden",
+  backgroundImage: "url(/* background-image.jpg */)",
+  backgroundSize: "contain",
+  backgroundRepeat: "no-repeat",
+  backgroundPosition: "center",
+}
